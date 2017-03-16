3441de55d83306890f4bf98777c97d6601c16312
Aubrey Franzen <franzena3@nku.edu>
2017-03-16 08:50:53 +0000

    Can add new users but have to direct to the page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314025813) do
+ActiveRecord::Schema.define(version: 20170316084356) do
 
   create_table "answers", force: :cascade do |t|
     t.string   "answer_text"
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 20170314025813) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "user_type"
-    t.integer  "reputation"
+    t.string   "reputation"
   end
 
 end
