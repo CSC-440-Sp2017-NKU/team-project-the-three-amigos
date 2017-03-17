ead9869e39e18a5aed8f9fbd93af369b79fa35ac
Matt Stulz <stulzm2@nku.edu>
2017-03-17 22:27:44 +0000

    Added new answer link to question, question show page, and question field within new answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -69,6 +69,6 @@ class AnswersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def answer_params
-      params.require(:answer).permit(:answer_text, :date, :user_id)
+      params.require(:answer).permit(:answer_text, :date, :user_id, :question_id)
     end
 end
