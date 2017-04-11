56636912000d3a1a31b767359a3140e1402a846d
Matt Stulz <stulzm2@nku.edu>
2017-04-11 15:35:49 +0000

    Reset captcha keys

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
-  config.public_key  = 'public key'
-  config.private_key = 'private key'
+  config.site_key  = '6LfkXhwUAAAAAK438hagTZpegZglKN4UXmcTsK8x'
+  config.secret_key = '6LfkXhwUAAAAABF0oUrFTnCJiwiPK8Brr1Z0hH-p'
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
