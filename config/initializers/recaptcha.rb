cc7ccd532b0efa5493e93a1ed48afa9d6a7bb145
Matt Stulz <stulzm2@nku.edu>
2017-04-11 15:23:00 +0000

    Changed keys for captcha heroku

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
-  config.site_key  = '6LfkXhwUAAAAAK438hagTZpegZglKN4UXmcTsK8x'
-  config.secret_key = '6LfkXhwUAAAAABF0oUrFTnCJiwiPK8Brr1Z0hH-p'
+  config.site_key  = ENV['RECAPTCHA_PUBLIC_KEY']
+  config.secret_key = ENV['RECAPTCHA_PRIVATE_KEY']
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
