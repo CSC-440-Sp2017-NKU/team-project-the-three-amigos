0adf9192b48496d7afe1d37e1d5e1ad52e1c32db
Aubrey Franzen <franzena3@nku.edu>
2017-04-18 22:27:31 +0000

    Paging for users and courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -5,7 +5,7 @@ class CoursesController < ApplicationController
   # GET /courses
   # GET /courses.json
   def index
-    @courses = Course.all
+    @courses = Course.paginate(:page => params[:page], :per_page => 10)
   end
   
   def import
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     end
     
     def index
-        @users = User.all 
+        @users = User.paginate(:page => params[:page], :per_page => 5) 
     end
     
     def import
