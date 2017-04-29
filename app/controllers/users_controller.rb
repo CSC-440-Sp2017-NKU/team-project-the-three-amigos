c6420b312ca6ec4dd1dcc602ba3ff168dd288dee
Lauren Hensley <lauren.marieh7@gmail.com>
2017-04-29 13:57:58 +0000

    Edit user type on update user now works.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,6 @@ class UsersController < ApplicationController
    end
     
     def user_params
-        params.require(:user).permit(:name, :email)
+        params.require(:user).permit(:name, :email, :faculty, :administrator)
     end
 end
